725ffdece106c114cda90949e6881a0b102ac6fd
Change19961103 <s1508767@ed.ac.uk>
2018-03-04 18:30:35 +0000

    update

--- line_detect.py
+++ line_detect.py
@@ -358,6 +358,7 @@ if __name__ == '__main__':
 
             ############################# send command to ev3 ###################
             s.sendMotorCommand(l_motor=left_motor, r_motor=right_motor)
+            s.sendMotorCommand(-200, -200)            
 
             ############################# output image TEST #####################
             # cv2.imshow('img_black',img_black)
