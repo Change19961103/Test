0e671678db69f79c3d28fc5d1c52e16d298872bd
Change19961103 <s1508767@ed.ac.uk>
2018-03-08 14:21:00 +0000

    update

--- line_detect.py
+++ line_detect.py
@@ -396,10 +396,14 @@ if __name__ == '__main__':
                     # line_following(some_color)
 
             # need color signal to specify turn left or right
-            if distance_Black:
+            if distance_Blue:
+                [left_motor, right_motor] = line.line_following(distance_Blue)
+                prev_l = left_motor
+                prev_r = right_motor
+            elif not distance_Blue:
                 [left_motor, right_motor] = line.line_following(distance_Black)
-                prev_r = left_motor
-                prev_l = right_motor
+                prev_l = left_motor
+                prev_r = right_motor
             else:
                 [left_motor, right_motor] = [-prev_l, -prev_r]
             print("left motor speed is {}".format(left_motor))
