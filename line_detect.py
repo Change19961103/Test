bf1aafeacd97235ba8749aad4ad8aec4b297de52
Change19961103 <s1508767@ed.ac.uk>
2018-03-13 22:57:48 +0000

    update

--- line_detect.py
+++ line_detect.py
@@ -440,6 +440,8 @@ if __name__ == '__main__':
                     [left_motor, right_motor] = line.line_following(distance_Black)
                     prev_l = left_motor
                     prev_r = right_motor
+                else:
+                    [left_motor, right_motor] = [-prev_l, -prev_r]
 
             s.sendMotorCommand(int(left_motor), int(right_motor))
             print("left motor speed is {}".format(left_motor))
